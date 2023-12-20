e82b1d9d20cb5f358dae31e296944c1c68e72ed3
Abukar-Hassan-Salah <abukarhassansalah@gmail.com>
2023-12-20 21:26:09 +0300

    updated data

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:emailpassword/dirid.dart';
 import 'package:emailpassword/firebase_options.dart';
 // import 'package:emailpassword/image.dart';
-import 'package:emailpassword/screen/widgets/sign_in.dart';
+// import 'package:emailpassword/screen/widgets/sign_in.dart';
 // import 'package:emailpassword/screen/widgets/sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
